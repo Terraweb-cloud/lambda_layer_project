df519765da760d32f1e6b465a21ee5640e4c68fb
Oluwaseun Musa <80710703+superissy@users.noreply.github.com>
2023-09-23 23:48:48 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,3 +9,18 @@ resource "aws_lambda_layer_version" "my_lambda_layer" {
   description     = "My Lambda Layer"
   compatible_runtimes = ["python3.8"]
 }
+
+
+# Define the Lambda function
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name = var.function_name
+  handler      = "main.lambda_handler"
+  runtime      = "python3.8"
+  role         = aws_iam_role.lambda_role.arn
+  filename     = "lambda_function/lambda_function.zip"
+
+  # Attach the Lambda Layer
+  layers = [var.layer_arn]
+}
+
+# Define an IAM role for the Lambda function (role definition not shown here)
